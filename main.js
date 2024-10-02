0141110ec1ac5e6954e2217d885e7df6f0dbdfd3
Mascarenhascherish <nnm23cs050@nmamit.in>
2024-10-03 05:26:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-const arithmetic=require('./arithmetic');
-const fs=require('fs');
-const a=10;
-const b=5;
+const arithmetic = require("./arithmetic");
+const fs = require("fs");
 
-const sum=arithmetic.add(a,b);
-const difference=arithmetic.subtract(a,b);
-const product=arithmetic.multiply(a,b);
-const quotient=arithmetic.divide(a,b);
+const a = 10;
+const b = 5;
+const sum = arithmetic.add(a, b);
+const difference = arithmetic.subtract(a, b);
+const product = arithmetic.multiply(a, b);
+const quotient = arithmetic.divide(a, b);
 
-console.log(`sum:${sum}`);
-console.log(`difference:${difference}`);
-console.log(`product:${product}`);
-console.log(`quotient:${quotient}`);
+console.log(Sum:${sum});
+console.log(Difference: ${difference});
+console.log(Product:${product});
+console.log(Quotient:${quotient});
 
-const results=
-sum:${sum}
-Difference:${difference}
-Product:${product}
-quotient:${quotient};
-fs.writeFile(`results.text`)
+const results = `
+    Sum:${sum}
+    Difference:${difference}
+    Product:${product}
+    Quotient:${quotient}
+`;
+
+fs.writeFile("results.txt", results, (err) => {
+  if (err) {
+    console.error("Error writng to file", err);
+  } else {
+    console.log("Results written to results.txt");
+  }
+});
+
+fs.readFile("results.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading from file", err);
+  } else {
+    console.log("Content of results.txt:");
+    console.log(data);
+  }
+});
